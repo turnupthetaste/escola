33ebc33d7a1108f603baa20747c62b9783e251ea
turnupthetaste <161386296+turnupthetaste@users.noreply.github.com>
2025-05-29 21:11:47 -0300

    Add files via upload

---
+++ escola-main/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import routes from './routes';
+import dotenv from "dotenv";
+import connect from "./models/connection";
+
+dotenv.config({ path: '/' });
+
+// será usado 3000 se a variável de ambiente não tiver sido definida
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.PORT);
+console.log(PORT);
+
+const app = express(); // cria o servidor e coloca na variável app
+// suportar parâmetros JSON no body da requisição
+app.use(express.json());
+
+// conecta ao MongoDB no início da aplicação
+connect();
+
+// inicializa o servidor na porta especificada
+app.listen(PORT, () => {
+    console.log(`Rodando na porta ${PORT}`);
+});
+
+// define a rota para o pacote /routes
+app.use(express.json());
+
+app.use(routes);
